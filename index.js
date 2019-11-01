768dd71454680c6df1dbec949a4275ca7de2d68d
Samantha Phipps <sammi.phipps@gmail.com>
2019-11-01 11:26:30 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(nameArray, eventName){
+    const newArray = [];
+    for(let i = 0; i < nameArray.length; i++){
+        newArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(positiveInteger){
+    while(positiveInteger >= 0){
+        console.log(positiveInteger);
+        positiveInteger--;
+    }
+}
